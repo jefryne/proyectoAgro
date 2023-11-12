d2c831195264ddb76928bea160c9191d5db95deb
Winder Roman <winderroman3131@gmail.com>
2023-11-12 17:02:20 +0100

    detectionv2

--- src/Api/getData.js
+++ src/Api/getData.js
@@ -12,7 +12,7 @@ export const getData = async () => {
 
 export const getDetection = async (file) => {
     try {
-        const endpoint = 'https://sambraulio.cognitiveservices.azure.com/customvision/v3.0/Prediction/340f9803-5747-40db-b94a-38d39a177ece/detect/iterations/detectionv1/image';
+        const endpoint = 'https://sambraulio.cognitiveservices.azure.com/customvision/v3.0/Prediction/340f9803-5747-40db-b94a-38d39a177ece/detect/iterations/detectionv2/image';
         const key = "609bcbd8f70d4b89aad75513331eb05e";
         const headers = {
             'Prediction-Key': key, 
